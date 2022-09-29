6da88a719fd61ff71e66a9f070d497d0260a69c6
GitHub Action <action@github.com>
2022-09-29 08:27:05 +0000

    Template cleanup

--- src/main/kotlin/com/github/maaref1/mvvmandroidarch/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/maaref1/mvvmandroidarch/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.maaref1.mvvmandroidarch.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.maaref1.mvvmandroidarch.MyBundle
 
 class MyApplicationService {
 
